9b0fa831482c56650fd4411a12b1011046b6e35f
Prashangsana <prashangsana.pvt@gmail.com>
2026-01-22 11:02:30 +0530

    update dashboard

--- client/src/dashboard/DashboardView.jsx
+++ client/src/dashboard/DashboardView.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DashboardView = () => {
     return (
         <div className="content-section fade-in">
-            <h2>Dashboard Overview</h2>
+            <h2>Dashboard Overview for You</h2>
             <h4>Welcome to your main feed.</h4>
         </div>
     );
