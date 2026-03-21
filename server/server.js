cf759f73f0ea344f4e0fc0b78fa4966795cd8671
Yahani Chalakshana <yahanichalakshana@gmail.com>
2026-03-21 14:56:28 +0530

    update FRONTEND_URL in .env and server.js for consistency

--- server/server.js
+++ server/server.js
@@ -18,8 +18,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: [FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'https://uniq.lk', 'https://www.uniq.lk'],
-  credentials: true, // This is crucial for cookies to work across domains!
+  origin: [FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174', 'https://uniq.lk', 'https://www.uniq.lk'],
+  credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
